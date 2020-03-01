e472b747a9fa908770018e34a7742290b537e8c2
keke0123 <keke0123@naver.com>
2020-03-01 09:02:43 +0900

    css builder / file builder / font builder

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,37 @@ const path = require("path");
 module.exports = {
     entry: "./src/index.js",
     output: {
-        filename: "main.js",
+        filename: "bundle.js",
         path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            // css builder
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            // file builder
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ["file-loader"]
+            },
+            // font builder
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: ["file-loader"]
+            },
+            //
+            {
+                test: /\.(csv|tsv)$/,
+                use: ["csv-loader"]
+            },
+            //
+            {
+                test: /\.xml$/,
+                use: ["xml-loader"]
+            }
+        ]
     }
 };
 
