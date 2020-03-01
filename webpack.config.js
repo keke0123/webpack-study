489667dbf4d4f7cf32aebaca6a4d97db1aa28e00
keke0123 <keke0123@naver.com>
2020-03-01 13:22:51 +0900

    development / mode 설정 / devtool inline-source-map / plugin devServer / express middleware 설정

--- webpack.config.js
+++ webpack.config.js
@@ -3,13 +3,19 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+    mode: "development", // 개발자 모드
+    devtool: "inline-source-map", // 개발자 도구에서 source map,
+    devServer: {
+        contentBase: "./dist"
+    },
     entry: {
         app: "./src/index.js",
         print: "./src/print.js"
     },
     output: {
         filename: "[name].bundle.js",
-        path: path.resolve(__dirname, "dist")
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "/"
     },
     module: {
         rules: []
